5c5c7c7c8b4425f6c3cc4e103f73627117f93751
wp1admin <82565331+wp1admin@users.noreply.github.com>
2021-04-15 14:10:54 +0800

    [NBUI] Initial commit

---
+++ react-native.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    dependencies: {
+        'react-native-code-push': {
+            platforms: {
+                // disable only on Android as we require some conditional setup
+                android: null,
+            },
+        },
+        'react-native-splash-screen': {
+            platforms: {
+                android: null,
+                ios: null,
+            },
+        },
+        'react-native-video': {
+            platforms: {
+              android: {
+                sourceDir: '../node_modules/react-native-video/android-exoplayer',
+              },
+            },
+        },
+    },
+};
